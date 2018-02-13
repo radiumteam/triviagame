be25387b33d09c830e13aecf723a971cd0a9ede8
Mejin Leechor <amleechor@gmail.com>
2018-02-12 20:00:22 -0800

    Create server routes and database functionality for trivia questions

--- server/models/questionModel.js
+++ server/models/questionModel.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const questionModel = new Schema({
-  category: String,
+const questionSchema = new Schema({
+  author_id: Schema.Types.ObjectId,
   question: {
     type: String,
     required: true
   },
+  answerOptions: { 
+    type: Array,
+    required: true
+  },
+  correctAnswerIndex: { 
+    type: Number,
+    required: true
+  },
+  category: String,
 });
+
+module.exports = mongoose.model('Question', questionSchema);
