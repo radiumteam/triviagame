133488bf8294b78bd8e5cdb3281fe9a9f22c1988
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-02-14 16:47:23 -0800

    modified userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -74,6 +74,7 @@ userController.verifyUser = (req, res, next) => {
 }
 
 userController.isLoggedIn = (req, res, next) => {
+  console.log('is loggedin', req.cookies.ssid)
   if (!req.cookies.ssid) {
     res.redirect('/')
   }
